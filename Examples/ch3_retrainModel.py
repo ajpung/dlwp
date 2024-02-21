68d2a6acea5d730b85f1ada04b393032f7f6a825
Aaron Pung <aaron.pung@gmail.com>
2024-02-21 07:54:30 -0500

    chore: continuing with reuters example, updated formats based on black

--- Examples/ch3_retrainModel.py
+++ Examples/ch3_retrainModel.py
@@ -8,26 +8,26 @@ from keras import losses, metrics
 
 (train_data, train_labels), (test_data, test_labels) = imdb.load_data(num_words=1000)
 
-def vectorize_sequences(sequences,dimension=10000):
-    results = np.zeros((len(sequences),dimension))
+
+def vectorize_sequences(sequences, dimension=10000):
+    results = np.zeros((len(sequences), dimension))
     for i, sequence in enumerate(sequences):
-        results[i,sequence] = 1.
+        results[i, sequence] = 1.0
     return results
+
 
 x_train = vectorize_sequences(train_data)
 x_test = vectorize_sequences(test_data)
-y_train = np.asarray(train_labels).astype('float32')
-y_test = np.asarray(test_labels).astype('float32')
+y_train = np.asarray(train_labels).astype("float32")
+y_test = np.asarray(test_labels).astype("float32")
 
 model = models.Sequential()
-model.add(layers.Dense(16, activation='relu', input_shape=(10000,)))
-model.add(layers.Dense(32, activation='relu'))
-model.add(layers.Dense(64, activation='relu'))
-model.add(layers.Dense(1, activation='sigmoid'))
+model.add(layers.Dense(16, activation="relu", input_shape=(10000,)))
+model.add(layers.Dense(32, activation="relu"))
+model.add(layers.Dense(64, activation="relu"))
+model.add(layers.Dense(1, activation="sigmoid"))
 
-model.compile(optimizer='rmsprop',
-              loss='mse',
-              metrics=['accuracy'])
+model.compile(optimizer="rmsprop", loss="mse", metrics=["accuracy"])
 
 model.fit(x_train, y_train, epochs=4, batch_size=512)
-results = model.evaluate(x_test,y_test)
+results = model.evaluate(x_test, y_test)
